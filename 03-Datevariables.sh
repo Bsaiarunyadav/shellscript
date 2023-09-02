7bd7c1b7bf121fafecc423d76b0dbc7ceeb8381b
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-02 22:53:11 +0530

    First commit

--- 03-Datevariables.sh
+++ 03-Datevariables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Normaly DATE is the command that it used for knowing the date and time of the command excuted.
+#Normaly DATE is the command that it used for knowing the date and time of the command excuted.
 
-To know when we excuted inside the script .
+#To know when we excuted inside the script .
 
 DATE=$(date)
 
-Now excute the command
+#Now excute command
 
 echo "Hello ,this command was excuted at the time stamp:$DATE".
