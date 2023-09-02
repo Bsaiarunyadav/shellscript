9f18b2f96ce4479e6310d7091cb85c4587bc4646
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-02 22:27:44 +0530

    First commit

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+
+echo "$PERSON1:hi $PERSON2"
+echo "$PERSON2:hi $PERSON1"
+echo "$PERSON1 :asked $PERSON2"
+echo "$PERSON2 :replyed to it yes $PERSON1"
+echo "$PERSON2 :said to $PERSON1"
