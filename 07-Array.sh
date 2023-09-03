9319a013c51ce0bfba86b54ce0043d23097d67f6
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-03 13:04:58 +0530

    First commit

---
+++ 07-Array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# DATA TYPES
+
+#Dont worry about data types ,shell script can intelligently understand .
+#1 Number is a general Data type 
+#2 boolean 0 & 1
+#  0 means - faliure 
+#  1 means - sucess
+# SIMILARLY we have Another Datatype called  "array" It mean that hold multiple values .
+
+PERSONS=("Ramesh" "suresh" "harish") #this is called array ,Instead single it can hold multiple values .
+#NOTE :- Inside array we always refers values with index and it starts from "0"
+
+echo "first person:${Persons[0]}" #this will show only 1 member that means RAMESH as index i.e "0"
