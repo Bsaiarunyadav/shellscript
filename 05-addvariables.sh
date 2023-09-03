5e2e287beb3697c5252070a04140775023d9f62d
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-03 12:21:13 +0530

    First commit

--- 05-addvariables.sh
+++ 05-addvariables.sh
@@ -10,7 +10,9 @@ NUMBER1=$1
 NUMBER2=$2
 
 # I need to run this command inside the shell to add 2 number now 
-#COMMAND = SUM=$((NUMBER1+NUMBER2)) Rember this is an arthematic equation.
+#COMMAND
+SUM=$((NUMBER1+NUMBER2)) 
+# Rember this is an arthematic equation.
 #By running the command it will not show the result as we not printed any thing so
 
 echo "Additon of 2 number is:$sum"
