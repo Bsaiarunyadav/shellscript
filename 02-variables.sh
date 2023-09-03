8ddd86394c7666a320fcf059f1ca1c8f37b7cbb1
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-03 13:25:47 +0530

    First commit

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#Declaring inside the script
+#When ever you want using instead of value , replace it with variable name .
+
+#Command=<variable-name>=<Value>
+
+
 
 PERSON1=arun
 PERSON2=abhinay
@@ -12,4 +16,6 @@ echo "$PERSON1 :asked $PERSON2"
 echo "$PERSON2 :replyed to it yes $PERSON1"
 echo "$PERSON2 :said to $PERSON1"
 
-
+#We have multiple types of variables :- 
+#1 Declaring inside the script ( as shown in above )
+#2 Another types is DATE -----> REFER 03-Variables.sh
--- 03-variables.sh
+++ 03-variables.sh
@@ -4,8 +4,12 @@
 
 #How To know when we excuted inside the script ? 
 
+#Now we need to get date & time inside the script to know at what time it was excuted at.
+
 DATE=$(date)
 
 #Now excute command
 
 echo "Hello ,this command was excuted at the time stamp:$DATE".
+
+#Above line  will show the date and time at what time it was excuted at . 
