084eeb35a085f9547dfef1825e852ebfbcbe9411
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-07 21:33:06 +0530

    First commit

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
 echo "Ramesh:hi suresh"
 echo "suresh:hi ramesh"
 echo "Ramesh :asked suresh"
 echo "Suresh :replyed to it yes ramesh"
 echo "Ramesh :said to suresh"
+
+# In the given excutions if we want to make any changes in furture it is not possible to change each and every single line 
+# So in variables ------> it follows a principle called "DRY principle."
+# DRY means Dont repeat your self .
+# if You feel any thing repeating again and again then create a variable and assign to a value to it.
+# CONTIUNE 02-Variables.sh
