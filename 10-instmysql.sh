728911a65f323dcba025fb926378812467234285
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-08 14:45:56 +0530

    First commit

--- 10-instmysql.sh
+++ 10-instmysql.sh
@@ -15,6 +15,26 @@ fi
 
 # as MYsqll is wrong it will show error and exit if 
 
+yum install mysql -y
 
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 2 
+else
+    echo "Installation of mysql is success"
+fi
 
 # And when we give mysql correctly it will run successfully
+
+#NOTE :- when ever we are running a command it is our respinsibity to validate it correctly 
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 2 
+else
+    echo "Installation of mysql is success"
+fi
