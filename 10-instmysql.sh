719b4fecdc7d7f0e07df8b273afcc4aeb4187619
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-08 14:46:37 +0530

    First commit

--- 10-instmysql.sh
+++ 10-instmysql.sh
@@ -2,16 +2,17 @@
 
 # Now we try to install mysql
 # Again it is our responsibity to check weather the installation was success or not .
-# If we give 
-yum install mysqll -y
+# If we give
+ 
+# yum install mysqll -y
 
-if [ $? -ne 0 ]
-then 
-    echo "Installation of mysql is error"
-    exit 2 
-else
-    echo "Installation of mysql is success"
-fi
+# if [ $? -ne 0 ]
+# then 
+#     echo "Installation of mysql is error"
+#     exit 2 
+# else
+#     echo "Installation of mysql is success"
+# fi
 
 # as MYsqll is wrong it will show error and exit if 
 
