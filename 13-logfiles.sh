23fec38cd494fcd821e3d415446bf0ee9d4eeec6
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-09 20:17:07 +0530

    First commit

---
+++ 13-logfiles.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Now let us improvise some more . When we run any code in return we get lot of points which are called as "LOGFILES"
+# EXAMPLE :-
+# When we run this-------> sudo sh 12-functions2.sh
+
+    # Last metadata expiration check: 19:11:17 ago on Fri 08 Sep 2023 02:12:15                                                           
+    # Package mysql-8.0.26-1.module_el8.4.0+915+de215114.x86_64 is already inst                                                          
+    # Dependencies resolved.
+    # Nothing to do.
+    # Complete!
+    # installing mysql...success
+    # Last metadata expiration check: 19:11:20 ago on Fri 08 Sep 2023 02:12:15                                                           
+    # Package postfix-2:3.5.8-7.el8.x86_64 is already installed.
+    # Dependencies resolved.
+    # Nothing to do.
+    # Complete!
+    # installing postfix...success
+
+# If you are writing any script "LOGFILES" are very important to know what happend .    
+# EX ---> when er rin any command and later if we "CLEAR" then the log files are gone .
+
+# SO the long files are very important to know what we are doing - So we should create a logfile to store them .
+# BUT For that we need to discuss something called "REDIRECTIONS".
+# EX :- ls - l
+        # 1
+        # 2   
+
+# Now for storing this in to log files 
+
+# COMMAND IS :- "ls -l > ls .log"  and check "cat ls .log"
+        # To remove log files --> "rm -rf ls .log"
+
+# ALSO Now if are giving and wrong command 
+#   EX :- "lsss -l > ls .log"
+# check :-   "cat ls .log"
+# The logs will not be shown as we printed wrong .
+# The **REDIRECTION** by default shows the "success OUTPUT".
+# In Redirections 
+        # 1  MEANS SUCCESS 
+        # 2  MEANS Failure 
+        # &  MEANS both success and failure .
+# HERE By default we no need to give "1"  as we know it was success  
+# EXAMPLE :- ls -l 1> ls .log
+# BUT when you give " lss -l 2>ls .log " It wont be shown & printed on screen . But it will be stored in "CAT ls.log " .
+# AS we know while practicing "Catalogue"
+    #   > --->  It will remove previous content and add new content 
+    #   >>----> It will not remove previos content But apend .
+#  EXAMPLE :- 
+        # lsss -l &>>ls .log 
+        # cat ls .log 
+    # again print 
+        # ls -l &>>ls .log
+        # cat ls .log 
+# Now Here the content was not getting removed but it was apending .
+# We should use ">>&" To redirect the success and failure  content .
+
---
+++ 14-special-characters.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#These are the special charecters given in default by linux
+
+VAR1=$1
+VAR2=$2
+
+echo "script name:$0"
+echo "variables:$VAR1"
+echo "All VAR:$@"
+echo "No of VARS:$#"
