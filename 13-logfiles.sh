ca96bee3eb295dade28c8a62334aafab960a061e
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-10 11:11:52 +0530

    First commit

--- 13-logfiles.sh
+++ 13-logfiles.sh
@@ -73,12 +73,12 @@ N="\e[0M"
 
 VALIDATE(){
     #$1--> It will receive the argument 1 
-    if [ $1 -ne 0 ]
-        then 
-            echo -e "$2 ...$R failure $N"
-            exit 2
-        else     
-            echo -e "$2 ...$G success $N"
+if [ $1 -ne 0 ]
+    then 
+        echo -e\ "$2 ...$R failure $N"
+        exit 2
+    else     
+        echo -e\ "$2 ...$G success $N"
     fi
 }
 
