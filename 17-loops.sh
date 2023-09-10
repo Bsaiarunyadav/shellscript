6b22912ee81ffeeec4a4a844eb82af0f861b67b2
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-10 16:47:23 +0530

    First commit

--- 17-loops.sh
+++ 17-loops.sh
@@ -2,10 +2,10 @@
 
 # Now we will install the packages using the loops.
 
-DATE=(date +%F)
-LOGDIR=/c/users/B.Radhaprathap/repo/shellscript
+DATE=$(date +%F)
+LOGSDIR=/c/users/B.Radhaprathap/repo/shellscript
 SCRIPT_NAME=$0
-LOGFILE=$LOGDIR/$0-$DATE.log
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
 N="\e[0m"
@@ -15,16 +15,16 @@ G="\e[32m"
 
 if [ $USERID -ne 0 ];
 then 
-    echo -e "ERROR :: installation is $R failure $N"
+    echo -e "$R ERROR :: installation is failure $N"
     exit 1
 fi
 
-VALIDATION(){
-    if [$1 -ne 0];
+VALIDATE(){
+    if [ $1 -ne 0 ];
     then 
-        echo -e "if Installing $2 ... $R FAILURE $N"
+        echo -e "Installing $2 ... $R FAILURE $N"
     else
-        echo -e "if installing $2 ... $G SUCCESS $N"
+        echo -e "installing $2 ... $G SUCCESS $N"
     fi
 }
 
