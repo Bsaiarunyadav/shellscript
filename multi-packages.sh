9a9faf4473aec0905fd5c5059d64da6ef7167cce
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-14 19:04:52 +0530

    First commit

--- multi-packages.sh
+++ multi-packages.sh
@@ -7,6 +7,6 @@ N=/e[0m"
 
 if [ $? -ne 0 ];
     then 
-        echo -e "$R ERROR :: Please run this script with root access $N"
+        echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
 fi
