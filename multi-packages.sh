c520eb5f6975654d0eb363b8dbdd0654329ee6c6
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-14 19:06:36 +0530

    First commit

--- multi-packages.sh
+++ multi-packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DATE=$(date +%F)
-
+USERID=$(id -u)
 R=/e[31m"
 N=/e[0m"
 
 
-if [ $? -ne 0 ];
+if [ $USERID -ne 0 ];
     then 
         echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1
