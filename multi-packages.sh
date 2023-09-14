dffeaafa9ac94a7bd1368fb057c152a1d665b597
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-14 11:02:57 +0530

    First commit

--- multi-packages.sh
+++ multi-packages.sh
@@ -3,7 +3,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 1000 ];
     then 
-        echo "NUMBER IS  GREATER THAN 1000"
+        echo "$NUMBER IS NOT GREATER THAN 1000"
     else 
-        echo "NUMBER IS NOT GREATER THAN 1000"
+        echo "$NUMBER IS GREATER THAN 1000"
 fi
