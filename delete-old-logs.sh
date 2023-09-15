584e8fe8bad8cb281ecce9200ade5ae090900ea0
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-15 19:11:58 +0530

    First commit

--- delete-old-logs.sh
+++ delete-old-logs.sh
@@ -5,4 +5,4 @@ SCRIPT_NAME=$0
 LOGSDIR=/home/centos/shellscript/shellscript-logs
 LOGSFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELTE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
-echo"$FILES_TO_DELTE"
+echo "$FILES_TO_DELTE"
