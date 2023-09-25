42ad731f241e2fb4519c423d84955afa48ba0e98
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-25 19:27:01 +0530

    First commit

--- del-old-logs.sh
+++ del-old-logs.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-APP_LOGS_DIR=/home/centos/app-logs
+APP_LOGS_DIR="/home/centos/shellscript/logfiles"
+DATE=$(date +%F)
+SCRIPT_NAME="$0"
+LOGSDIR="/home/centos/shellscript/shellscript-logs"
+LOGSFILE="$LOGSDIR/$SCRIPT_NAME-$DATE.log"
+FILES_TO_DELETE=$(find "$APP_LOGS_DIR" -name "*.log" -type f -mtime +10)
+echo "$FILES_TO_DELETE"
 
-DATE=$(date +%F:%H:%M:%S)
-LOGSDIR=/home/centos/shellscript/shellscript-logs
-# /home/centos/shellscript-logs/script-name-date.log
-SCRIPT_NAME=$0
-LOGFILE=$LOGSDIR/$0-$DATE.log
-
-FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
-
-echo "script started executing at $DATE" &>>$LOGFILE
-
-while read line
+while read -r line
 do
-    echo "Deleting $line" &>>$LOGFILE
-    rm -rf "$line"
-done <<< $FILES_TO_DELETE
+    echo "Deleting $line" >> "$LOGSFILE"
+    rm -f "$line" # Remove the -r option
+done <<< "$FILES_TO_DELETE"
