a2cc4c395b308c8aae9a67f2ed32233ade66b6e6
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-09-30 21:58:08 +0530

    First commit

---
+++ mail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#any one in your project can call this script with arguments .
+
+TO_ADDRESS=$1
+SUBJECT=$2
+BODY=$3
+TEAM_NAME=$4
+ALERT_TYPE=$5
+
+# HOW to call this script
+# mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE 
+
+echo "all args:$@"
