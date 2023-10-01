85dd3a8f9c50153f9bd46298d674b5dc8c0d22a6
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-10-01 13:07:16 +0530

    First commit

--- mail.sh
+++ mail.sh
@@ -10,5 +10,8 @@ ALERT_TYPE=$5
 
 # HOW to call this script
 # mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE 
+FINAL_BODY=$(sed -e '/TEAM_NAME/DEVOPS-TRANING/g' 's/ALERT_TYPE/High disk usage/g' -e "s/MESSAGE/$BODY"  template.HTML)
 
-echo "all args:$@"
+#echo "all args:$@"
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
