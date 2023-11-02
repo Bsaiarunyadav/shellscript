4eeb6e5c3c3efe4b12e89a71c8f198decd6f193b
Bsaiarunyadav <bsaiyadav1234@gmail.com>
2023-11-02 20:09:51 +0530

    createdchange

--- 07-Array.sh
+++ 07-Array.sh
@@ -9,7 +9,7 @@
 #  1 means - sucess
 # SIMILARLY we have Another Datatype called  "array" It mean that hold multiple values .
 
-PERSONS=("Ramesh" "suresh" "harish") #this is called array ,Instead single it can hold multiple values .
+#this is called array ,Instead single it can hold multiple values .
 #NOTE :- Inside array we always refers values with index and it starts from "0"
 
 echo "first person:${PERSONS[0]}" #this will show only 1 member that means RAMESH as index i.e "0"
